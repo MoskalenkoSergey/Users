a10b136776b0878dce0e72c013c304e525c34248
MoskalenkoSergey <moserg04@gmail.com>
2023-09-29 09:54:25 +0300

    Добавьте файлы проекта.

---
+++ UsersWPF/Avtor.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UsersWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Avtor.xaml
+    /// </summary>
+    public partial class Avtor : Page
+    {
+        public Avtor()
+        {
+            InitializeComponent();
+        }
+    }
+}
